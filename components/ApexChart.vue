6f414a216a952863989f95aa92badee389035b96
Tinashe Zvihwati <tinashetp@gmail.com>
2020-11-17 15:47:33 +0300

    removes_max_on_y

--- components/ApexChart.vue
+++ components/ApexChart.vue
@@ -1,14 +1,16 @@
 <template>
-  <div id="chart">
-    <client-only>
-      <apexchart
-        type="line"
-        height="520"
-        :options="data.chartOptions"
-        :series="data.series"
-      ></apexchart>
-    </client-only>
-  </div>
+  <section>
+    <div id="chart">
+      <client-only>
+        <apexchart
+          type="line"
+          height="500"
+          :options="data.chartOptions"
+          :series="data.series"
+        ></apexchart>
+      </client-only>
+    </div>
+  </section>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,18 +2,18 @@
   <div class="small">
     <h1 class="title">
       Стоимость квадратного метра в квартире площадью 41-54, 99 м2
-      <br><span>в евро vs. в унциях золоте</span>
+      <br /><span>в евро vs. в унциях золоте</span>
     </h1>
-    <ApexChart v-bind:data="{chartOptions, series}"/>
+    <ApexChart v-bind:data="{chartOptions, series}" />
   </div>
 </template>
 
 <script>
-import series from "@/assets/json/series.json"
-import chartOptions from "@/assets/json/chartOptions.json"
+import series from "@/assets/json/series.json";
+import chartOptions from "@/assets/json/chartOptions.json";
 export default {
   components: {
-    'ApexChart': () => import('@/components/ApexChart')
+    ApexChart: () => import("@/components/ApexChart"),
   },
   data() {
     return {
@@ -34,29 +34,29 @@ export default {
             formatter: function (y) {
               var value = y;
               if (typeof y !== "undefined") {
-                if(y > 500) value =  '€ ' + y;
+                if (y > 500) value = "€ " + y;
               }
               return value;
             },
           },
         },
-      },
+      }
     };
-  },
+  }
 };
 </script>
 
 <style>
-  .small .title{
-    margin: auto;
-    text-align: center;
-    font-weight: 400;
-    font-size: 22px;
-    max-width: 60%;
-    margin-top: 20px;
-    line-height: 1;
-  }
-  .small .title span{
-    font-size: 16px;
-  }
+.small .title {
+  margin: auto;
+  text-align: center;
+  font-weight: 400;
+  font-size: 22px;
+  max-width: 60%;
+  margin-top: 20px;
+  line-height: 1;
+}
+.small .title span {
+  font-size: 16px;
+}
 </style>
